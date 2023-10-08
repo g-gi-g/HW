550e3c3b98faff8e7473ac8f45f4fe21a5eb9549
Yehor_Yelenin <eelenin568@gmail.com>
2023-10-08 23:59:22 +0300

    HW-5

---
+++ HW-5/Properties/Properties/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+class Student
+{
+    private static string name;
+
+    public static string Name
+    {
+        set { name = value; }
+        get { return; }
+    }
+}
+
+internal class Program
+{
+    static void Main() 
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Student.Name = "Іван";
+        Console.WriteLine("Ім'я студента: " +  Student.Name);
+        Console.ReadKey();
+    }
+    
+}
